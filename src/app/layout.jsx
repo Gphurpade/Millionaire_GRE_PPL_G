0d610bbecfa1d852c2083939bbd3f47a50cca366
AryTinu <shendearyan966@gmail.com>
2026-01-23 16:50:14 +0530

    adding route

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Navbar from "./components/common/Navbar";
-import Footer from "./components/common/Footer";
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
 
 export default function RootLayout({ children }) {
   return (
